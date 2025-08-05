a129592843b8667288d412b5c83b9fdd05fd76af
PriceLlewellyn <price13.llewellyn@gmail.com>
2025-08-05 16:20:14 +0100

    set out routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from "dotenv"
+import routes from '../routes';
 
 dotenv.config()
 
@@ -11,7 +12,7 @@ app.use(cors())//* accepts request from anywhere
 app.use(express.json())//accepst json
 app.use(express.urlencoded({extended: true}))
 
-//app.use("/api-v1", routes);
+app.use("/api-v1", routes);
 
 app.use("*", (req, res) => {
     res.status(404).json({
