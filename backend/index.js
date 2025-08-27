d07834f82ecdbca41b8d16d98c2f51d37e907190
PriceLlewellyn <price13.llewellyn@gmail.com>
2025-08-27 09:07:09 +0100

    Cleaned out backend/started again

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,37 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from "dotenv";
-//import routes from '../routes';
-import routes from './routes/index.js';
+import pool from './src/config/db.js';
+
 
 // Load enviroment variables into process.env
-dotenv.config()
+dotenv.config();
 
 // creates express application
 const app = express();
 
 // Define the port number (use environment variable if set, otherwise default to 8000)
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 
 // Middlewares
 app.use(cors()) // Allow requests from any origin (public API access)
 app.use(express.json()) // Parse incoming requests with JSON payloads
-app.use(express.urlencoded({extended: true})) // Parse URL-encoded request bodies (e.g., from HTML forms)
 
 
 // Routes
-app.use("/api-v1", routes); // Mount all API routes under "/api-v1" path
 
-// Catch-all route for undefined endpoints (404 handler)
-app.use("*", (req, res) => {
-    res.status(404).json({
-        status: "404 not found",
-        message: "Route not found"
-    });
+
+// Error Handling middleware
+
+// Testing POSTGRESS Connection
+app.get("/", async(requestAnimationFrame, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The databse name is: ${result.rows[0].current_database}`);
 });
 
+
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running on http:localhost:${port}`);
 });
