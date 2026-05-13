12e96136c0f9e21796f343ea6e65d53b8c7affd0
PriceLlewellyn <price13.llewellyn@gmail.com>
2026-05-13 08:51:46 +0100

    feat: users REST Api template laid out

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaService } from './prisma/prisma.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { UsersModule } from './modules/users/users.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
