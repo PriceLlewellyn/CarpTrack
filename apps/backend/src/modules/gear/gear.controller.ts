ce5cb276ae48e3aad4922db1bd54596cda7a5e82
PriceLlewellyn <price13.llewellyn@gmail.com>
2026-05-17 15:18:27 +0100

    feat: gear api built out to a basic standard, more conditions to be added further down line

--- apps/backend/src/modules/gear/gear.controller.ts
+++ apps/backend/src/modules/gear/gear.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { GearService } from './gear.service';
 import { CreateGearDto } from './dto/create-gear.dto';
 import { UpdateGearDto } from './dto/update-gear.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { GearEntity } from './entities/gear.entity';
 
 @Controller('gear')
 export class GearController {
@@ -9,26 +11,38 @@ export class GearController {
 
   @Post()
   create(@Body() createGearDto: CreateGearDto) {
-    return this.gearService.create(createGearDto);
+    const userId = 1;
+
+    return this.gearService.create(userId, createGearDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: GearEntity, isArray: true })
   findAll() {
     return this.gearService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.gearService.findOne(+id);
+    const userId = 1;
+    
+    return this.gearService.findOne(+id, userId);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: GearEntity })
   update(@Param('id') id: string, @Body() updateGearDto: UpdateGearDto) {
     return this.gearService.update(+id, updateGearDto);
   }
 
   @Delete(':id')
+  @ApiCreatedResponse({ type: GearEntity })
   remove(@Param('id') id: string) {
     return this.gearService.remove(+id);
   }
 }
+
+//endpoint to tie catches to a specific gear
+//endpioint to show favourite brand
+//endpoint to display the brand comparision
+//endpoint to suggest real reel power to type of fishing
--- apps/backend/src/modules/gear/gear.service.ts
+++ apps/backend/src/modules/gear/gear.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateGearDto } from './dto/create-gear.dto';
 import { UpdateGearDto } from './dto/update-gear.dto';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class GearService {
-  create(createGearDto: CreateGearDto) {
-    return 'This action adds a new gear';
+  constructor(private _prisma: PrismaService) {}
+
+  create(userId: number, _createGearDto: CreateGearDto) {
+    return this._prisma.gear.create({
+      data: {
+        ..._createGearDto,
+        user: {
+          connect: {id: userId}
+        }
+    }
+  });
   }
 
   findAll() {
-    return `This action returns all gear`;
+    return this._prisma.gear.findMany({ 
+      where: { 
+        type: "Reel" }});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} gear`;
+  findOne(id: number, userId: number) {
+    const gear = this._prisma.gear.findFirst({
+      where: {
+        id, userId
+      }
+    });
+    if (!gear) {
+      throw new NotFoundException('Gear noot found or you do not have permission.');
+    }
+    return gear;
   }
 
-  update(id: number, updateGearDto: UpdateGearDto) {
-    return `This action updates a #${id} gear`;
+  update(id: number, _updateGearDto: UpdateGearDto) {
+    return this._prisma.user.update({
+          where: {id},
+          data: UpdateGearDto,
+        });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} gear`;
+    return this._prisma.gear.delete({ where: { id } });
   }
 }
